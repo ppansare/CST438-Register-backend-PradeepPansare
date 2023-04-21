d11a9fb930e2bf11b03c10a7a4268365f74dcd8a
Pradeep <pansarepradeep15@gmail.com>
2023-04-21 02:17:20 -0700

    Student Controller Student DTO Junit Tests Student

--- cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
@@ -35,9 +35,11 @@ public class GradebookServiceMQ extends GradebookService {
 		 
 		// TODO 
 		// create EnrollmentDTO and send to gradebookQueue
+		EnrollmentDTO enrollmentDTO = new EnrollmentDTO(student_email, student_name, course_id);
+		rabbitTemplate.convertAndSend(gradebookQueue.getName(), enrollmentDTO);
 
 		
-		System.out.println("Message send to gradbook service for student "+ student_email +" " + course_id);  
+		System.out.println("Message send to gradebook service for student "+ student_email +" " + course_id);
 		
 	}
 	
@@ -47,6 +49,12 @@ public class GradebookServiceMQ extends GradebookService {
 
 		//TODO 
 		// for each student grade in courseDTOG,  find the student enrollment entity, update the grade and save back to enrollmentRepository.
+		for (CourseDTOG.GradeDTO g : courseDTOG.grades) {
+			Enrollment e = enrollmentRepository.findByEmailAndCourseId(g.student_email, courseDTOG.course_id);
+			e.setCourseGrade(g.grade);
+			enrollmentRepository.save(e);
+			System.out.println("Final grade update " + g.student_email + " " + courseDTOG.course_id + " " + g.grade);
+		}
 
 	}
 
